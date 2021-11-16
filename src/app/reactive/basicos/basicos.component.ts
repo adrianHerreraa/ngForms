2c860747537f333e65d95333346cf389b14e160d
AdrianHerrera_ <neomach55@gmail.com>
2021-11-15 23:39:07 -0600

    Reactive, basicos y dinamicos

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,6 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +10,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // miFormulario: FormGroup = new FormGroup({
+  //   lblNombre: new FormControl('RTX 4080ti'),
+  //   lblPrecio: new FormControl(0),
+  //   lblExistencias: new FormControl(0),
+  // });
 
-  ngOnInit(): void {
+  miFormulario: FormGroup = this.fb.group({
+    lblNombre: [
+      , 
+      [
+        Validators.required,
+        Validators.minLength(5),
+      ],
+    ],
+    lblPrecio: [
+      ,
+      [
+        Validators.required,
+        Validators.min(0),
+      ],
+    ],
+    lblExistencias: [
+      ,
+      [
+        Validators.required,
+        Validators.min(0),
+      ],
+    ]
+  });
+
+  constructor( private fb: FormBuilder) { }
+
+  ngOnInit(){
+
+    // Si utilizas la función setValue, debes de mandar los campos exactamente igual al formGroup definido arriba.
+
+    // this.miFormulario.setValue({
+    //   lblNombre: 'RTX 4080ti',
+    //   lblPrecio: 1800,
+    //   lblExistencias: 7,
+    // });
+
+    // Mejor se utiliza reset, que permite poner los campos que gustes sin obligarte a poner todos.
+    this.miFormulario.reset({
+      lblNombre: 'RTX 4080ti',
+      lblPrecio: 1800,
+    });
+
+  }
+
+  isFieldValid( campo: string ){
+    return this.miFormulario.controls[campo].errors 
+    && this.miFormulario.controls[campo].touched;
+  }
+
+  onSave(){
+
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }else{
+      console.log(this.miFormulario.value);
+      this.miFormulario.reset();
+    }
+    
   }
 
 }
--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, Validator, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -8,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DinamicosComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    lblNombre:[
+      '', 
+      [ 
+        Validators.required, 
+        Validators.minLength(5), 
+      ],
+    ],
+    favoritos: this.fb.array([
+      ['Metal Gear', Validators.required], 
+      ['Death Stranding', Validators.required],
+      ],
+      Validators.required,
+    ),
+  });
+
+  nuevoFavorito: FormControl = this.fb.control('', Validators.required);
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  fieldIsValid( label: string ){
+    return this.miFormulario.controls[label].touched 
+      && this.miFormulario.controls[label].errors;
+  }
+
+  onSave(){
+    if(this.miFormulario.invalid){
+      this.miFormulario.markAllAsTouched();
+      return;
+    }else{
+      console.log(this.miFormulario.value);
+      // this.miFormulario.reset();
+    }
+  }
+  
+  agregarFavorito(){
+    
+    if(this.nuevoFavorito.invalid){
+      return;
+    }
+
+    // this.favoritosArr.push( new FormControl( this.nuevoFavorito.value, Validators.required ));
+    this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required ) );
+
+    this.nuevoFavorito.reset();
+  }
+
+  borrar( index: number ){
+    console.log('Borrarrrr' + index);
+    this.favoritosArr.removeAt(index);
   }
 
 }
