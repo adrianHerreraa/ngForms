5032bc2a97f3f92780066eda78e41d1a37c29187
AdrianHerrera_ <neomach55@gmail.com>
2021-11-17 00:21:27 -0600

    errores personalizados

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,80 +1,104 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from '@angular/forms';
 // import { emailPattern, nombreApellidoPatter, noPuedeserNeoMach } from 'src/app/shared/validators/validaciones';
 import { ValidatorService } from '../../../shared/validators/validator.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class RegistroComponent implements OnInit {
+  miFormulario: FormGroup = this.fb.group(
+    {
+      lblNombre: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.vService.nombreApellidoPatter),
+        ],
+      ],
+      lblEmail: [
+        '',
+        [Validators.required, Validators.pattern(this.vService.emailPattern)],
+        [this.emailValidator],
+      ],
+      lblUsername: ['', [Validators.required, this.vService.noPuedeserNeoMach]],
+      lblPassword: ['', [Validators.required, Validators.minLength(6)]],
+      lblPassword2: ['', [Validators.required]],
+    },
+    {
+      validators: [this.vService.camposIguales('lblPassword', 'lblPassword2')],
+    }
+  );
 
-  miFormulario: FormGroup = this.fb.group({
-    lblNombre: [
-      '', 
-      [ 
-        Validators.required, 
-        Validators.pattern( this.vService.nombreApellidoPatter ) 
-      ],
-    ],
-    lblEmail: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern( this.vService.emailPattern ),
-      ],
-    ],
-    lblUsername: [
-      '',
-      [
-        Validators.required,
-        this.vService.noPuedeserNeoMach,
-      ]
-    ],
-    lblPassword: [
-      '',
-      [
-        Validators.required,
-        Validators.minLength(6),
-      ]
-    ],
-    lblPassword2: [
-      '',
-      [
-        Validators.required,
-      ]
-    ],
+  get emailErrorMsg(): string{
+
+    const errors = this.miFormulario.get('lblEmail')?.errors;
     
-  },
-  {
-    validators: [
-      this.vService.camposIguales('lblPassword', 'lblPassword2'),
-    ]
-  });
+    if( errors?.required ){
+      return '* El campo email es obligatorio';
+    }else if( errors?.pattern){
+      return '* El formato de email no es válido';
+    }else if( errors?.emailTomado ){
+      return '* El email ya está en uso';
+    }else{
+      return '';
+    }
+  }
 
-  constructor( 
+  constructor(
     private fb: FormBuilder,
     private vService: ValidatorService,
-  ) { }
+    private emailValidator: EmailValidatorService
+  ) {}
 
   ngOnInit(): void {
     this.miFormulario.reset({
       lblNombre: 'Luis Adrian',
-      lblEmail: 'neomch55@gmail.com',
+      lblEmail: 'test1@test.com',
       lblUsername: 'K4rma',
+      lblPassword: '123456',
+      lblPassword2: '123456',
     });
   }
 
-  fieldIsValid( campo: string ){
-    return this.miFormulario.get(campo)?.invalid
-      && this.miFormulario.get(campo)?.touched;
+  fieldIsValid(campo: string) {
+    return (
+      this.miFormulario.get(campo)?.invalid &&
+      this.miFormulario.get(campo)?.touched
+    );
   }
 
-  onSubmit(){
+  // emailRequired(){
+  //   return (
+  //     this.miFormulario.get('lblEmail')?.errors?.required &&
+  //     this.miFormulario.get('lblEmail')?.touched
+  //   );
+  // }
+
+  // emailPattern(){
+  //   return (
+  //     this.miFormulario.get('lblEmail')?.errors?.pattern &&
+  //     this.miFormulario.get('lblEmail')?.touched
+  //   );
+  // }
+
+  // emailTomado(){
+  //   return (
+  //     this.miFormulario.get('lblEmail')?.errors?.emailTomado &&
+  //     this.miFormulario.get('lblEmail')?.touched
+  //   );
+  // }
+
+  onSubmit() {
     console.log(this.miFormulario.value);
     this.miFormulario.markAllAsTouched();
   }
-
 }
